f3651c6d4dce5ae041842fc2f76fb8c3b3316ad1
nandaafd <nandaafd.info@gmail.com>
2023-10-12 19:12:12 +0700

    modified route

--- routes/api.php
+++ routes/api.php
@@ -18,30 +18,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::namespace('Api')->group(function(){
+
     Route::prefix('auth')->group(function(){
         Route::post('login', [AuthController::class, 'login'])->name('login');
         Route::post('register', [RegisterController::class, 'register'])->name('register');
     });
     Route::middleware('auth:api')->group(function(){
         Route::post('logout',[AuthController::class, 'logout']);
-        Route::prefix('news')->group(function(){
-            Route::get('list', [NewsController::class, 'index']);
-            Route::get('{id}', [NewsController::class, 'show']);
-            Route::post('store', [NewsController::class, 'store']);
-            Route::put('{id}/update', [NewsController::class, 'update']);
-            Route::delete('{id}/delete', [NewsController::class, 'destroy']);
-        });
-        Route::prefix('comment')->group(function(){
-            Route::get('list', [CommentController::class, 'index']);
-            Route::post('create',[CommentController::class, 'store']);
-            Route::get('{id}', [CommentController::class, 'show']);
-            Route::put('{id}/update', [CommentController::class, 'update']);
-            Route::delete('{id}/delete', [CommentController::class, 'destroy']);
-        });
+        Route::resource('news', NewsController::class);
+        Route::resource('comment', CommentController::class);
     });
-    
-    
-   
-});
+
 
